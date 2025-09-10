60bb84fbfc467757afbdc0b52be47caddc4574de
tripti-khandelwal <triptikhandelwal4321@gmail.com>
2025-09-10 13:22:44 +0530

    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ecommerce-website/",
+  // base: "/ecommerce-website/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
